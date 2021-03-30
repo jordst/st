e1f3be849c2690580201c73f71bf73ec8c020a99
jord <alrial24@posteo.net>
2021-03-30 03:07:46 +0100

    source

---
+++ hb.h
@@ -0,0 +1,9 @@
+#include <X11/Xft/Xft.h>
+#include <hb.h>
+#include <hb-ft.h>
+#include "/usr/include/harfbuzz/hb-ft.h"
+#include "/usr/include/harfbuzz/hb.h"
+
+void hbunloadfonts();
+void hbtransform(XftGlyphFontSpec *, const Glyph *, size_t, int, int);
+
